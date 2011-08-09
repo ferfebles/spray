e5a4affbcc924a2daa4b73cb3a9475ea6dd7468d
ferfebles <ferfebles@gmail.com>
2011-08-09 19:12:15 +0100

    Mixing debug and control port code

--- lib/controllers/rdebug_controller.rb
+++ lib/controllers/rdebug_controller.rb
@@ -2,41 +2,42 @@ require 'net/telnet'
 
 class RDebugController
   RDEBUG_NO_PRINT= File.expand_path(File.join(File.dirname(__FILE__), "rdebug_no_print.rb"))
-  RDEBUG_PROMPT= /PROMPT \(rdb:\d*\) |CONFIRM Really quit\? \(y\/n\)/
+  PROMPT= {:rdebug=> /PROMPT \(rdb:\d*\) |CONFIRM Really quit\? \(y\/n\)/, :control=> /PROMPT \(rdb:ctrl\)/}
   @@current_port= 31415
   
   attr_reader :launch_debugger_command
   
   def initialize(path, host='127.0.0.1', port=nil)
-    @connected, @remote_debug, @remote_control= false, nil, nil
-    @path, @host, @port = path, host, (port || @@current_port+=2)
-    @launch_debugger_command= "rdebug -nx -p #{@port} --cport #{@port+1} -s -w -r '#{RDEBUG_NO_PRINT}' '#{@path}'"
+    @connected, @remote= false, {:rdebug=> nil, :control=> nil}
+    @path, @host, @port = path, host, {:rdebug=> (port || @@current_port+=2)}
+    @port[:control]= @port[:rdebug]+1
+    @launch_debugger_command= "rdebug -nx -p #{@port[:rdebug]} --cport #{@port[:control]} -s -w -r '#{RDEBUG_NO_PRINT}' '#{@path}'"
   end
   
-  def connect
-    retries= 1 #1 seconds of timeout
+  def connect(remote_port=:rdebug)
+    retries= 10
     begin
-      @remote_debug= Net::Telnet::new("Host"=> @host, "Port"=> @port, "Prompt"=> RDEBUG_PROMPT,
-      "Telnetmode"=> false, "Timeout"=> 1, "Waittime"=> 0)
+      @remote[remote_port]= Net::Telnet::new("Host"=> @host, "Port"=> @port[remote_port],
+      "Prompt"=> PROMPT[remote_port], "Telnetmode"=> false, "Timeout"=> 1, "Waittime"=> 0)
     rescue
-      (sleep 0.1; retry) if 0<=(retries-=0.1)
+      (sleep 0.1; retry) if 0<=(retries-=1)
       @connected= false
-      raise("Timeout connecting to rdebug on #{@host}:#{@port}")
+      raise("Timeout connecting to rdebug on #{@host}:#{@port[remote_port]}")
     end
-    @remote_debug.waitfor(/PROMPT \(rdb:\d*\)/)
+    @remote[remote_port].waitfor(PROMPT[remote_port])
     @connected= true
   end
   
   def connect_control
-    retries= 10 #1 seconds of timeout
+    retries= 10
     begin
-      @remote_control= Net::Telnet::new("Host"=> @host, "Port"=> @port+1, "Prompt"=> /PROMPT \(rdb:ctrl\)/,
-      "Telnetmode"=> false, "Timeout"=> 10, "Waittime"=> 1)
+      @remote_control= Net::Telnet::new("Host"=> @host, "Port"=> @port[:control],
+      "Prompt"=> PROMPT[:control], "Telnetmode"=> false, "Timeout"=> 1, "Waittime"=> 1)
     rescue
-      (sleep 1; retry) if 0<=(retries-=1)
-      raise("Timeout connecting to rdebug control on #{@host}:#{@port+1}")
+      (sleep 0.1; retry) if 0<=(retries-=1)
+      raise("Timeout connecting to rdebug control on #{@host}:#{@port[:control]}")
     end
-    @remote_control.waitfor(/PROMPT \(rdb:ctrl\)/)
+    @remote_control.waitfor(PROMPT[:control])
   end
   
   def execute_command(command)
@@ -45,8 +46,7 @@ class RDebugController
       file, line= command.scan(/'(.*)':(\d+)/).first
       toggle_breakpoint(file ,line.to_i)
     when /^\s*interrupt/
-      puts "before interrupt"
-      control_interrupt
+      send_command(command, :control)
     else
       send_command(command)
     end
@@ -64,39 +64,30 @@ class RDebugController
   def control_interrupt
     begin
       puts "interrupt"
-      @remote_control.cmd('interrupt') || ''
+      @remote[:control].cmd('interrupt') || ''
     rescue
-      connect_control; retry
+      connect(:control); retry
     end
   end
   
   def current_breakpoints
-    #if connected?
-      send_command("info break").scan(/(\/.*):(\d*)/).map{|file,line| [file,line.to_i]} rescue []
-      #else
-      #[]
-      #end
+    send_command("info break").scan(/(\/.*):(\d*)/).map{|file,line| [file,line.to_i]} rescue []
   end
   
   # Returns array with current position: [[file, line]]
   def current_position
-    #    if connected?
-      send_command("info line").scan(/Line\s(\d+).*"(.*)"/).map{|line,file| [file,line.to_i]} rescue []
-      #else
-      #[]
-      #end
+    send_command("info line").scan(/Line\s(\d+).*"(.*)"/).map{|line,file| [file,line.to_i]} rescue []
   end
   
   def connected?
     @connected
   end
   
-  def send_command(command)
+  def send_command(command, remote_port=:rdebug)
     begin
-      puts command
-      @remote_debug.cmd(command) || ''
+      @remote[remote_port].cmd(command) || ''
     rescue
-      connect; retry
+      connect(remote_port); retry
     end
   end
   
